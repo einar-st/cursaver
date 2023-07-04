2e5596a8e517c7f728980f902677c67db96facac
Einar Quizon Stavnes <estavnes@gmail.com>
2023-07-04 18:40:58 +0200

    Game of Life loop detection

--- game_of_life.py
+++ game_of_life.py
@@ -2,7 +2,7 @@
 # - loop detection
 
 import curses
-from funcs import draw_ch, wrap, i2y, i2x
+from funcs import draw_ch, draw_str, cycle, wrap, i2y, i2x
 import random
 
 
@@ -36,28 +36,34 @@ def gol_init(maxx, maxy):
     data['maxy'] = maxy
     data['rate'] = 60
 
+    data['snaps'] = [[], []]
+    data['snap_idx'] = 0
+    data['snap_cnt'] = 0
+
     curses.init_pair(1, curses.COLOR_GREEN, -1)
     curses.init_pair(2, curses.COLOR_RED, -1)
     curses.init_pair(3, curses.COLOR_BLUE, -1)
-    data['cols'] = {
-        'std': curses.color_pair(0),
-        'green': curses.color_pair(1),
-        'red': curses.color_pair(2),
-        'blue': curses.color_pair(3)
-    }
 
-    data['cells'] = []
-    for i in range(maxx * maxy):
-        data['cells'].append(random.randint(0, 1))
+    data['cells'] = [random.randint(0, 1) for _ in range(maxx * maxy)]
     return data
 
 
 def gol_update(data, stdscr):
+
     maxx = data['maxx']
     maxy = data['maxy']
     cells = data['cells']
-    cols = data['cols']
     flip_queue = []
+
+    # repopulate if in closed loop
+    if data['snap_cnt'] == 15:  # snapshot every 15th iteration
+        data['snaps'][data['snap_idx']] = cells.copy()
+        data['snap_idx'] = cycle(data['snap_idx'], 1)
+        if data['snaps'][0] == data['snaps'][1]:
+            data['cells'] = [random.randint(0, 1) for _ in range(maxx * maxy)]
+        data['snap_cnt'] = 0
+    else:
+        data['snap_cnt'] += 1
 
     # add cells to flip-queue and draw live cells
     for cell in range(maxx * maxy):
@@ -69,18 +75,18 @@ def gol_update(data, stdscr):
         if cells[cell] >= 1 and (count == 2 or count == 3):
             if cells[cell] == 1:
                 draw_ch('#', y, x, stdscr, 1)
+                cells[cell] += 1
             else:
                 draw_ch('#', y, x, stdscr)
-            cells[cell] += 1
         elif (
             (cells[cell] >= 1 and (count < 2 or count > 3))
             or (cells[cell] == 0 and count == 3)
         ):
             flip_queue.append(cell)
             if cells[cell] > 1:
-                draw_ch('#', y, x, stdscr, 1)
-            elif cells[cell] == 1:
                 draw_ch('#', y, x, stdscr, 2)
+            elif cells[cell] == 1:
+                draw_ch('#', y, x, stdscr, 1)
 
     # flip cells
     for cell in flip_queue:
